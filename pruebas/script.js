a919c09f66d715eded5b3b71b62c49a04e0face6
rubenbase <daggosso@hotmail.com>
2019-01-21 18:05:21 +0100

    Added app.js and removed some logs

--- pruebas/script.js
+++ pruebas/script.js
@@ -1,11 +1,4 @@
-/*
- * Aquí podéis hacer los ejercicios y
- * practicar Javascript!
- */
-
 const red = document.querySelector("#red");
-
-console.log(red);
 
 function setProperty(duration) {
   red.style.animationDuration = Math.floor(Math.random() * 5 + 1) + "s";
